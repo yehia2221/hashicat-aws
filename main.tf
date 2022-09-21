7f1b5ee1e395445d6155bd8b31122eb3d4997f54
Yahya <yahya.aldhuraibi@gmail.com>
2022-09-21 10:55:44 +0000

    tags 2

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "Billable"
+    Department = "test"
+    Billable = "yes"
   }
 }
 
